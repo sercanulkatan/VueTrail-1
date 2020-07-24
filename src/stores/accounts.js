452130e867327de20092f28dfa537a908c516eb0
sercanulkatan <sercanulkatan@gmail.com>
2020-07-24 14:22:11 +0300

    Düzenleme actions

--- src/stores/accounts.js
+++ src/stores/accounts.js
@@ -22,29 +22,34 @@ const getters = {
     }
 };
 
-const actions={
-    initApp({commit}){
-        sAccount._getAccounts().then(d=>{
+const actions = {
+    initApp({
+        commit
+    }) {
+        sAccount._getAccounts().then(d => {
             state.Account.key = d.key;
             state.Account.Balance = d.Balance;
             state.Account.Sold = d.Sold;
             state.Account.Recieved = d.Recieved;
         })
     },
-    async updateAccounts({commit,state},prms){
+    async updateAccounts({
+        commit,
+        state
+    }, prms) {
         var done = false;
         if (prms.type == 1) { //Satım
-            state.Account.Balance = Math.round(parseFloat(state.Account.Balance) + parseFloat(prms.val));
-            state.Account.Sold = Math.round(parseFloat(state.Account.Sold) + parseFloat(prms.val));
+            state.Account.Balance = parseFloat(state.Account.Balance) + parseFloat(prms.val);
+            state.Account.Sold = parseFloat(state.Account.Sold) + parseFloat(prms.val);
         } else if (prms.type == 2) { //Alım
-            state.Account.Balance = Math.round(parseFloat(state.Account.Balance) - parseFloat(prms.val));
-            state.Account.Recieved = Math.round(parseFloat(state.Account.Recieved) + parseFloat(prms.val));
+            state.Account.Balance = parseFloat(state.Account.Balance) - parseFloat(prms.val);
+            state.Account.Recieved = parseFloat(state.Account.Recieved) + parseFloat(prms.val);
         } else if (prms.type == 3) { //İade
-            state.Account.Balance = Math.round(parseFloat(state.Account.Balance) + parseFloat(prms.val));
-            state.Account.Recieved = Math.round(parseFloat(state.Account.Recieved) - parseFloat(prms.val));
+            state.Account.Balance = parseFloat(state.Account.Balance) + parseFloat(prms.val);
+            state.Account.Recieved = parseFloat(state.Account.Recieved) - parseFloat(prms.val);
         }
-        await sAccount._updateAccounts(state.Account).then(res=>{
-            done=res;
+        await sAccount._updateAccounts(state.Account).then(res => {
+            done = res;
         });
         return done;
     }
--- src/stores/product.js
+++ src/stores/product.js
@@ -42,12 +42,12 @@ const getters = {
 };
 
 const mutations = {
-    updateProductList(state,product){
+    updateProductList(state, product) {
         state.Products.push(product);
     },
-    deleteProduct(state,key){
-        const ind = state.Products.findIndex(x=>x.key==key);
-        state.Products.splice(ind,1);
+    deleteProduct(state, key) {
+        const ind = state.Products.findIndex(x => x.key == key);
+        state.Products.splice(ind, 1);
     },
     async getProductList() {
         var a = await sProduct._getProductList();
@@ -65,49 +65,66 @@ const mutations = {
     }
 };
 
-const actions={
-    async saveProduct({dispatch,commit,state},p){
-        var done=false;
+const actions = {
+    async saveProduct({
+        dispatch,
+        commit,
+        state
+    }, p) {
+        var done = false;
         p.Cost = p.Price;
-        p.Price = Math.round(parseFloat(p.Price) + (Math.round((parseFloat(p.Price) * 10) / 100)));
-        await sProduct._insertProduct(p).then(async res=>{
-            if(res!=""){
-                p.key=res;
-                commit("updateProductList",p);
-                await dispatch("updateAccounts", {type:2, val:parseFloat(p.Cost) * p.Count}).then(res=>{
-                    if(!res) done=false;
-                    else done=true;
+        p.Price = parseFloat(p.Price) + ((parseFloat(p.Price) * 10) / 100);
+        await sProduct._insertProduct(p).then(async res => {
+            if (res != "") {
+                p.key = res;
+                commit("updateProductList", p);
+                await dispatch("updateAccounts", {
+                    type: 2,
+                    val: parseFloat(p.Cost) * p.Count
+                }).then(res => {
+                    if (!res) done = false;
+                    else done = true;
                 });
-            }
-            else done=false
-        }).catch((ex)=>{
-            done=false;
+            } else done = false
+        }).catch((ex) => {
+            done = false;
         });
         return done;
     },
-    async deleteProducts({dispatch,commit},key){
+    async deleteProducts({
+        dispatch,
+        commit
+    }, key) {
         const pr = state.Products.filter(x => x.key == key)[0];
-        var res = {message:"",done:false};
-        if(pr.Count>0){
-        await dispatch("updateAccounts", {type:3, val:(Math.round(parseFloat(pr.Count) * parseFloat(pr.Cost)))})
-            .then(async acRes=>{
-                if (acRes) {
-                    await sProduct._deleteAll(key).then(async done=>{
-                        res.message="Silme işlemi başarılı.";
-                        res.done = true;
-                        await commit("deleteProduct",key);
-                    });
-                } else {
-                    await dispatch("updateAccounts", {type:1, val:(Math.round(parseFloat(pr.Count) * parseFloat(pr.Cost)))})
-                    .then(res=>{
-                        res.message="Ürün silinemedi.";
-                        res.done = false;
-                    });
-                }
-            });
-        }
-        else{
-            res.message="Silinecek ürün bulunamadı.";
+        var res = {
+            message: "",
+            done: false
+        };
+        if (pr.Count > 0) {
+            await dispatch("updateAccounts", {
+                    type: 3,
+                    val: (parseFloat(pr.Count) * parseFloat(pr.Cost))
+                })
+                .then(async acRes => {
+                    if (acRes) {
+                        await sProduct._deleteAll(key).then(async done => {
+                            res.message = "Silme işlemi başarılı.";
+                            res.done = true;
+                            await commit("deleteProduct", key);
+                        });
+                    } else {
+                        await dispatch("updateAccounts", {
+                                type: 1,
+                                val: (parseFloat(pr.Count) * parseFloat(pr.Cost))
+                            })
+                            .then(res => {
+                                res.message = "Ürün silinemedi.";
+                                res.done = false;
+                            });
+                    }
+                });
+        } else {
+            res.message = "Silinecek ürün bulunamadı.";
             res.done = false;
         }
         return res;
